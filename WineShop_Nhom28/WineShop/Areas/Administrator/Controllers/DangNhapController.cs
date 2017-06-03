85e140eb7acb26a1b3c7f8872d42070cf0ff5155
minhvu <vominhvu96@gmail.com>
2017-06-03 11:58:07 +0700

    Cập nhật toàn bộ
    - Thay đổi tên project
    - Cập những thay đổi lớn so với project cũ

---
+++ WineShop_Nhom28/WineShop/Areas/Administrator/Controllers/DangNhapController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using WineShop.Models;
+
+namespace WineShop.Areas.Administrator.Controllers
+{
+    
+    public class DangNhapController : Controller
+    {
+        // GET: Administrator/DangNhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public  ActionResult Index(string username , string password)
+        {
+            ShopRuouDBEntities db = new ShopRuouDBEntities ();
+            
+            var result = db.AspNetUsers.Single(r => r.Id.Equals("minda-admin-min-ad"));
+            bool check = VerifyHashedPassword(result.PasswordHash, password);
+            if(!check)
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            Session["DangNhap"] = "true";
+            return RedirectToAction("Index", "SanPham");
+        }
+        public ActionResult Thoat()
+        {
+            Session["DangNhap"] = null;
+            return RedirectToAction("Index", "DangNhap");
+        }
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            //return ByteArraysEqual(buffer3, buffer4);
+            return buffer3.SequenceEqual(buffer4);
+        }
+    }
+}
---
+++ WineShop_Nhom28/WineShop/Areas/Administrator/Controllers/KhachHangController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WineShop.Models;
+using PagedList;
+using System.Net;
+using System.Data.Entity;
+
+namespace WineShop.Areas.Administrator.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        ShopRuouDBEntities db = new ShopRuouDBEntities();
+        // GET: Administrator/KhachHang
+        public ActionResult Index(string sortOrder, string currentFilter, string TimKhachHang, int? page)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            Session["a"] = "KhachHang";
+            ViewBag.CurrentFilter = sortOrder;
+            ViewBag.TenKhachHangs = string.IsNullOrEmpty(sortOrder) ? "khachhang" : "khachhang_desc";
+
+            var kh = db.AspNetUsers.Where(h => h.LockoutEnabled == false );
+      
+            if (sortOrder != null && sortOrder.Equals("khachhang_desc"))
+            {
+                kh = kh.OrderByDescending(h => h.UserName);
+            }
+            else
+            {
+                kh = kh.OrderBy(h => h.UserName);
+            }
+            if (TimKhachHang != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                TimKhachHang = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = TimKhachHang;
+            if (TimKhachHang != null)
+            {
+                kh = kh.Where(h => h.UserName.Contains(TimKhachHang.Trim()));
+            }
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(kh.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult PhanQuyen(string id)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var kh = db.AspNetUsers.Single(u => u.Id.Equals(id));
+
+            var role = db.AspNetRoles.ToList();
+            List<AspNetRole> quyens = new List<AspNetRole>();
+            foreach(var r in role)
+            {
+                if (!kh.AspNetRoles.Contains(r))
+                {
+                    quyens.Add(r);
+                }
+            }
+            if(quyens.Count == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            ViewBag.Quyens = new SelectList(quyens, "Id", "Name");
+            return View(kh);
+        }
+        [HttpPost]
+        public ActionResult PhanQuyen([Bind(Include = "Id")]AspNetUser user, string RoleID)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+           
+
+            // có thể không cần truy vấn 2 dòng dưới
+            
+            var kh = db.AspNetUsers.Single(u => u.Id.Equals(user.Id));
+            var tempRole = new AspNetRole
+            {
+                Id = RoleID
+            };
+            bool exists = kh.AspNetRoles.Contains(tempRole);
+            if (exists)
+            {
+                return View(user);
+            }
+            // New class vừa tạo
+            AspNetUserRoles a = new AspNetUserRoles();
+            //kh.ID có thể truyền trực tiếp bằng mã/id không cần load từ Db Lên
+            var khachhang = a.Users.Find(user.Id); //<<== Chỗ này
+            var quyen = a.Roles.Find(RoleID);//<<== Chỗ này
+            khachhang.AspNetRoles.Add(quyen);
+            //class vừa tạo SaveChanges()
+            if (a.SaveChanges() > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                List<AspNetRole> listQuyen = db.AspNetRoles.ToList<AspNetRole>();
+                ViewBag.Quyens = new SelectList(listQuyen, "Id", "Name");
+                return View(user);
+            }
+
+
+        }
+        public ActionResult HuyPhanQuyen(string id)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var kh = db.AspNetUsers.Single(u => u.Id.Equals(id));
+            List<AspNetRole> quyen = kh.AspNetRoles.ToList<AspNetRole>();
+            ViewBag.Quyens = new SelectList(quyen, "Id", "Name");
+            return View(kh);
+        }
+        [HttpPost]
+        public ActionResult HuyPhanQuyen([Bind(Include = "Id")]AspNetUser user, string RoleID)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            var kh = db.AspNetUsers.Single(u => u.Id.Equals(user.Id));
+            
+            // nếu 1 khách hàng chỉ còn 1 quyên thì không được xóa quyền nữa
+            if (kh.AspNetRoles.Count == 1)
+            {
+                return RedirectToAction("Index");
+            }
+            AspNetUserRoles a = new AspNetUserRoles();
+            var khachhang = a.Users.Find(user.Id);
+            var quyen = a.Roles.Find(RoleID);
+            khachhang.AspNetRoles.Remove(quyen);
+            if (a.SaveChanges() > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                List<AspNetRole> listQuyen = kh.AspNetRoles.ToList<AspNetRole>();
+                ViewBag.Quyens = new SelectList(listQuyen, "Id", "Name");
+                return View(user);
+            }
+        }
+
+        public ActionResult Xoa([Bind(Include = "Id")]AspNetUser user)
+        {
+            if (Session["DangNhap"] == null || !Session["DangNhap"].ToString().Equals("true"))
+            {
+                return RedirectToAction("Index", "DangNhap");
+            }
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            var kh = db.AspNetUsers.Single<AspNetUser>(h => h.Id == user.Id);
+            
+            int donDatHangCount = kh.DonDatHangs.Count;
+            int quyenCount = kh.AspNetRoles.Count;
+            if(donDatHangCount == 0 && quyenCount == 1 )
+            {
+                // do trong chỉ có 1 row trong bảng mối quan hệ nhiều - nhiều
+                // nên có thể lấy bằng lệnh Single
+                // mục đích lấy đc Id  mà ApsNetRole
+                var xoaQuyen = kh.AspNetRoles.Single();
+                
+                AspNetUserRoles a = new AspNetUserRoles();
+                //tìm trong bảng phụ khách hàng
+                var _khachhang = a.Users.Find(kh.Id);
+                // tìm 1 quyền duy nhất của khách hàng
+                var _quyen = a.Roles.Find(xoaQuyen.Id);
+                // xóa khách hàng ra khỏi bảng phụ
+                _quyen.AspNetUsers.Remove(_khachhang);
+                // save lại bảng phụ
+                a.SaveChanges();
+                // lúc này khách hàng đã hết Role nên có thể xóa.
+                db.AspNetUsers.Remove(kh);
+            }else
+            {
+                // nếu khách hàng có đơn hàng -> xóa ẩn
+                kh.LockoutEnabled = true;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
