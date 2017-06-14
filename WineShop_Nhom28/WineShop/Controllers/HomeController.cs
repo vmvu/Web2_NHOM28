38f05e4c722546ca7ec202e0599fbf0d15bde1b2
1461448 <longts96@gmail.com>
2017-06-14 19:28:46 +0700

    Them session bi khoa

--- WineShop_Nhom28/WineShop/Controllers/HomeController.cs
+++ WineShop_Nhom28/WineShop/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WineShop.Models;
 
 namespace WineShop.Controllers
 {
@@ -11,23 +12,47 @@ namespace WineShop.Controllers
     {
         public ActionResult Index()
         {
-            Session["BiKhoa"] = null;
+            //Session["BiKhoa"] = null;
+            if (Session["BiKhoa"] != null)
+            {
+                return RedirectToAction("LogOffs", "Account");
+            }
             string MaTK = null;
             MaTK = User.Identity.GetUserId();
             if (MaTK != null)
             {
                 Session["DangNhap"] = "1";
             }
+
+            if(Session["DangKyRoles"] != null && Session["DangKyRoles"].ToString().Equals("DKRoles"))
+            {
+                String userIDnek = User.Identity.GetUserId();
+                AspNetUserRoles userR = new AspNetUserRoles();
+                var u = userR.Users.Find(userIDnek);
+                var _quyen = userR.Roles.Find("1");
+                u.AspNetRoles.Add(_quyen);
+                userR.SaveChanges();
+                Session["DangKyRoles"] = null;
+            }
+
             return View();
         }
 
         public ActionResult HienThiThongTin()
         {
+            if (Session["BiKhoa"] != null)
+            {
+                return RedirectToAction("LogOffs", "Account");
+            }
             return View();
         }
 
         public ActionResult HienThiLienHe()
         {
+            if (Session["BiKhoa"] != null)
+            {
+                return RedirectToAction("LogOffs", "Account");
+            }
             return View();
         }
     }
